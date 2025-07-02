7926f70aa71979188dbb96527b498085debb4a7c
Miguel Mantoan Castellani <140843719+miguelcastell@users.noreply.github.com>
2025-07-02 17:44:33 -0300

    Add files via upload

---
+++ tabelas.py
@@ -0,0 +1,56 @@
+tabelas_simples = {
+    "III": [
+        {"limite": 180000.00, "aliquota": 6.0, "deducao": 0.0},
+        {"limite": 360000.00, "aliquota": 11.2, "deducao": 9360.00},
+        {"limite": 720000.00, "aliquota": 13.5, "deducao": 17640.00},
+        {"limite": 1800000.00, "aliquota": 16.0, "deducao": 35640.00},
+        {"limite": 3600000.00, "aliquota": 21.0, "deducao": 125640.00},
+        {"limite": 4800000.00, "aliquota": 33.0, "deducao": 648000.00}
+    ],
+    "IV": [
+        {"limite": 180000.00, "aliquota": 4.5, "deducao": 0.0},
+        {"limite": 360000.00, "aliquota": 9.0, "deducao": 8100.00},
+        {"limite": 720000.00, "aliquota": 10.2, "deducao": 12420.00},
+        {"limite": 1800000.00, "aliquota": 14.0, "deducao": 39780.00},
+        {"limite": 3600000.00, "aliquota": 22.0, "deducao": 183780.00},
+        {"limite": 4800000.00, "aliquota": 33.0, "deducao": 828000.00}
+    ],
+    "V": [
+        {"limite": 180000.00, "aliquota": 15.5, "deducao": 0.0},
+        {"limite": 360000.00, "aliquota": 18.0, "deducao": 4500.00},
+        {"limite": 720000.00, "aliquota": 19.5, "deducao": 9900.00},
+        {"limite": 1800000.00, "aliquota": 20.5, "deducao": 17100.00},
+        {"limite": 3600000.00, "aliquota": 23.0, "deducao": 62100.00},
+        {"limite": 4800000.00, "aliquota": 30.5, "deducao": 540000.00}
+    ]
+}
+
+distribuicao = {
+    "III": {
+        "IRPJ": 0.04,
+        "CSLL": 0.04,
+        "COFINS": 0.285,
+        "PIS": 0.06,
+        "CPP": 0.335,
+        "ISS": 0.235,
+        "PD": 0.0
+    },
+    "IV": {
+        "IRPJ": 0.21,
+        "CSLL": 0.12,
+        "COFINS": 0.141,
+        "PIS": 0.03,
+        "CPP": 0.0,
+        "ISS": 0.499,
+        "PD": 0.0
+    },
+    "V": {
+        "IRPJ": 0.335,
+        "CSLL": 0.155,
+        "COFINS": 0.16,
+        "PIS": 0.035,
+        "CPP": 0.0,
+        "ISS": 0.315,
+        "PD": 0.0
+    }
+}
